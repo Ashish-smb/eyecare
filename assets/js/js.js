b2cb34388532d007cc382e6bf08bd9f8c0f62e1d
Ashish-smb <ashishkumar2004smb@gmail.com>
2024-07-14 23:54:10 +0530

    eyecare-home

---
+++ assets/js/js.js
@@ -0,0 +1,15 @@
+$('.hero-area-eyecare').owlCarousel({
+    items: 1,
+    loop :true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    dots:true,
+    nav:true,
+});
+$('.eye-service-carousel').owlCarousel({
+    items:4,
+    margin:20,
+    autoplay:true,
+    loop:true,
+});
